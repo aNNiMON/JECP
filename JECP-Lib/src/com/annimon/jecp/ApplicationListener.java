a0bb247e4daed44f81648dad98e733d833e22142
Victor <melnikukr@mail.ru>
2014-01-30 21:07:55 +0200

    Add ApplicationListener

---
+++ JECP-Lib/src/com/annimon/jecp/ApplicationListener.java
@@ -0,0 +1,14 @@
+package com.annimon.jecp;
+
+/**
+ *
+ * @author aNNiMON
+ */
+public interface ApplicationListener {
+    
+    public void onCreate();
+    
+    public void onPause();
+    
+    public void onExit();
+}
