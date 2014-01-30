c0c2890f3600824aafe729cbb021307f0edda335
Victor <melnikukr@mail.ru>
2014-01-30 20:42:46 +0200

    Add JECP Android library project

---
+++ JECP-Android/src/com/annimon/jecp/JecpMainActivity.java
@@ -0,0 +1,12 @@
+package com.annimon.jecp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JecpMainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
