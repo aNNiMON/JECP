52fa2c36338d5891d7b7790a263baf4db0c2016e
Victor <melnikukr@mail.ru>
2014-01-31 11:31:54 +0200

    Add methods to JecpGraphics

--- JECP-Lib/src/com/annimon/jecp/JecpGraphics.java
+++ JECP-Lib/src/com/annimon/jecp/JecpGraphics.java
@@ -6,5 +6,11 @@ package com.annimon.jecp;
  */
 public interface JecpGraphics {
     
+    public void drawLine(int x1, int y1, int x2, int y2);
+    
     public void drawRect(int x, int y, int width, int height);
+    
+    public void fillRect(int x, int y, int width, int height);
+    
+    public void setColor(int color);
 }
