2691dea57bf25684b54dbe3687ff2f61f5062a9a
Victor <melnikukr@mail.ru>
2014-01-31 11:33:06 +0200

    Implement methods in JecpGraphics ME

--- JECP-ME/src/com/annimon/jecp/me/JecpGraphics.java
+++ JECP-ME/src/com/annimon/jecp/me/JecpGraphics.java
@@ -13,9 +13,20 @@ public class JecpGraphics implements com.annimon.jecp.JecpGraphics {
     protected JecpGraphics(Graphics g) {
         this.g = g;
     }
+    
+    public void drawLine(int x1, int y1, int x2, int y2) {
+        g.drawLine(x1, y1, x2, y2);
+    }
 
     public void drawRect(int x, int y, int width, int height) {
         g.drawRect(x, y, width, height);
     }
     
+    public void fillRect(int x, int y, int width, int height) {
+        g.fillRect(x, y, width, height);
+    }
+
+    public void setColor(int color) {
+        g.setColor(color);
+    }
 }
