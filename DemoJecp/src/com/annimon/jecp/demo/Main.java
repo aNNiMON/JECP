35a3c4242068684ccb9acc4f8d39b19c0854b945
Victor <melnikukr@mail.ru>
2014-01-31 20:50:20 +0200

    Add highlight cell to demo

--- DemoJecp/src/com/annimon/jecp/demo/Main.java
+++ DemoJecp/src/com/annimon/jecp/demo/Main.java
@@ -9,14 +9,20 @@ import com.annimon.jecp.ApplicationListener;
  */
 public class Main implements ApplicationListener {
     
+    private static final int CELLS_COUNT = 30;
+    
     private int width, height;
     private DynamicLine[] lines;
+    
+    private int cellX, cellY, cellColor;
+    private long cellUpdateTime;
 
     public void onStartApp(int width, int height) {
         this.width = width;
         this.height = height;
         System.out.println("width: " + width + "  height: " + height);
         
+        initHighlightCell();
         lines = new DynamicLine[5];
         for (int i = 0; i < lines.length; i++) {
             lines[i] = new DynamicLine(
@@ -36,17 +42,18 @@ public class Main implements ApplicationListener {
     public void onPaint(JecpGraphics g) {
         g.setColor(0xFF000000);
         g.fillRect(0, 0, width, height);
-        final int count = 20;
-        final int areaSize = width / count;
-        final int colorStep = 255 / (count + 1);
-        for (int i = 0; i < count; i++) {
-            for (int j = 0; j < count; j++) {
+        final int areaSize = Math.min(width, height) / CELLS_COUNT;
+        final int colorStep = 255 / (CELLS_COUNT + 1);
+        for (int i = 0; i < CELLS_COUNT; i++) {
+            for (int j = 0; j < CELLS_COUNT; j++) {
                 int color = colorStep * Math.min(i, j);
                 color = 0xFF000000 | (color << 16) | (color << 8) | color;
                 g.setColor(color);
                 g.drawRect(i * areaSize, j * areaSize, areaSize, areaSize);
             }
         }
+        g.setColor(cellColor);
+        g.fillRect(cellX * areaSize + 1, cellY * areaSize + 1, areaSize - 1, areaSize - 1);
         for (int i = 0; i < lines.length; i++) {
             lines[i].onPaint(g);
         }
@@ -56,10 +63,21 @@ public class Main implements ApplicationListener {
         for (int i = 0; i < lines.length; i++) {
             lines[i].onUpdate(width, height);
         }
+        if ((System.currentTimeMillis() - cellUpdateTime) > 1000) {
+            // Update every second.
+            initHighlightCell();
+        }
         try {
             Thread.sleep(20L);
         } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
+    }
+    
+    private void initHighlightCell() {
+        cellX = JecpRandom.rand(CELLS_COUNT);
+        cellY = JecpRandom.rand(CELLS_COUNT);
+        cellColor = JecpRandom.randomColor(0, 255);
+        cellUpdateTime = System.currentTimeMillis();
     }
 }
