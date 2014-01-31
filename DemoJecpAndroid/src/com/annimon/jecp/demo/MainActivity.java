8a3afaccfa2c6993465b07e2cfb177409f7592c0
Victor <melnikukr@mail.ru>
2014-01-31 20:18:49 +0200

    Add demo android project

---
+++ DemoJecpAndroid/src/com/annimon/jecp/demo/MainActivity.java
@@ -0,0 +1,11 @@
+package com.annimon.jecp.demo;
+
+import com.annimon.jecp.android.JecpApplication;
+
+public class MainActivity extends JecpApplication {
+
+    @Override
+    protected void onCreate() {
+        init(new Main());
+    }
+}
