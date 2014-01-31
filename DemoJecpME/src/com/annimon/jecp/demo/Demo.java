13966d6f68fc646b7a1e5dd876b60ba84cc942f7
Victor <melnikukr@mail.ru>
2014-01-31 20:19:12 +0200

    Add demo microedition project

---
+++ DemoJecpME/src/com/annimon/jecp/demo/Demo.java
@@ -0,0 +1,14 @@
+
+package com.annimon.jecp.demo;
+
+import com.annimon.jecp.me.JecpApplication;
+
+/**
+ * @author aNNiMON
+ */
+public class Demo extends JecpApplication {
+
+    public Demo() {
+        super(new Main());
+    }
+}
