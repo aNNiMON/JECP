e10e7916ed6af2ff9da3a9cf48f4693c7704fa59
Victor <melnikukr@mail.ru>
2014-02-01 17:14:01 +0200

    Add keys class

---
+++ JECP-Lib/src/com/annimon/jecp/Keys.java
@@ -0,0 +1,76 @@
+
+package com.annimon.jecp;
+
+/**
+ * Keyboard codes.
+ * @author aNNiMON
+ */
+public class Keys {
+    
+    public static boolean wasdAsDpad = false;
+    public static boolean numericdAsDpad = false;
+    
+    public static final int
+            DPAD_LEFT = -501,
+            DPAD_UP = -502,
+            DPAD_RIGHT = -503,
+            DPAD_DOWN = -504,
+            DPAD_FIRE = -505;
+    
+    /** Java ME keys */
+    public static final int
+            KEY_NUM0 = 48,
+            KEY_NUM1 = 49,
+            KEY_NUM2 = 50,
+            KEY_NUM3 = 51,
+            KEY_NUM4 = 52,
+            KEY_NUM5 = 53,
+            KEY_NUM6 = 54,
+            KEY_NUM7 = 55,
+            KEY_NUM8 = 56,
+            KEY_NUM9 = 57,
+            KEY_STAR = 42,
+            KEY_POUND = 35;
+    
+    /** Java SE keys */
+    public static final int
+            VK_LEFT = 37,
+            VK_UP = 38,
+            VK_RIGHT = 39,
+            VK_DOWN = 40,
+            VK_ENTER = 10,
+            VK_SPACE = 32,
+            VK_W = 87,
+            VK_S = 83,
+            VK_A = 65,
+            VK_D = 68;
+    
+    public static final int convertToDpad(int key) {
+        if (wasdAsDpad) {
+            switch(key) {
+                case VK_W: return DPAD_UP;
+                case VK_S: return DPAD_DOWN;
+                case VK_A: return DPAD_LEFT;
+                case VK_D: return DPAD_RIGHT;
+                case VK_ENTER: return DPAD_FIRE;
+                case VK_SPACE: return DPAD_FIRE;
+            }
+        }
+        if (numericdAsDpad) {
+            switch(key) {
+                case KEY_NUM2: return DPAD_UP;
+                case KEY_NUM8: return DPAD_DOWN;
+                case KEY_NUM4: return DPAD_LEFT;
+                case KEY_NUM6: return DPAD_RIGHT;
+                case KEY_NUM5: return DPAD_FIRE;
+            }
+        }
+        switch(key) {
+            case VK_UP: return DPAD_UP;
+            case VK_DOWN: return DPAD_DOWN;
+            case VK_LEFT: return DPAD_LEFT;
+            case VK_RIGHT: return DPAD_RIGHT;
+        }
+        return 0;
+    }
+}
