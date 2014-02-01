dd603623a1f69e187034e77ff2fb09d3efa7e264
Victor <melnikukr@mail.ru>
2014-02-01 11:56:38 +0200

    Add InputListener interface to core

---
+++ JECP-Lib/src/com/annimon/jecp/InputListener.java
@@ -0,0 +1,22 @@
+
+package com.annimon.jecp;
+
+/**
+ *
+ * @author aNNiMON
+ */
+public interface InputListener {
+    
+    public void onKeyPressed(int key);
+    
+    public void onKeyReleased(int key);
+    
+    public void onKeyRepeated(int key);
+    
+    
+    public void onPointerPressed(int x, int y);
+    
+    public void onPointerReleased(int x, int y);
+    
+    public void onPointerDragged(int x, int y);
+}
