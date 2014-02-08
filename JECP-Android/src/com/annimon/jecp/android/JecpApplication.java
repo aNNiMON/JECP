8a3e72f705c75a997db89dfd72465241ff9d7354
Victor <melnikukr@mail.ru>
2014-02-08 11:40:47 +0200

    Fix image initializer in Android

--- JECP-Android/src/com/annimon/jecp/android/JecpApplication.java
+++ JECP-Android/src/com/annimon/jecp/android/JecpApplication.java
@@ -23,6 +23,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import com.annimon.jecp.ApplicationListener;
+import com.annimon.jecp.JecpImage;
 
 public abstract class JecpApplication extends Activity {
     
@@ -37,6 +38,7 @@ public abstract class JecpApplication extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                              WindowManager.LayoutParams.FLAG_FULLSCREEN);
         sAssetManager = getAssets();
+        JecpImage.imageInitializer = new JecpImageInitializer();
         onCreate();
     }
     
